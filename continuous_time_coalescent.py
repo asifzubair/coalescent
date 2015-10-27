20e0cccf21b75404301b645628d245fb9ffc8881
asif zubair <asifzuba@usc.edu>
2015-10-27 12:07:10 -0700

    CT coalescent model

---
+++ continuous_time_coalescent.py
@@ -0,0 +1,29 @@
+"""
+Continuous time coalescenet
+Algorithm 1 - Page 24 
+"""
+
+
+from numpy.random import exponential as exp
+from numpy.random import choice as sample
+
+
+N = range(1,11)
+k = len(N)
+
+while k > 1:
+
+	T = exp(k*(k-1)/2.)
+
+	ix = sample(range(k),2,replace=False)
+
+	g0 = [item for i,item in enumerate(N) if i not in ix]
+	g1 = [N[i] for i in ix]
+
+	g0.append(g1)
+	N = g0
+
+	print T, N
+
+	k = k-1
+
