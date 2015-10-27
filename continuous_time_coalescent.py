b8fe5b04f53c5c9ca7d000f82f9aa19b5b563eb6
Asif Zubair <asif.zubair@gmail.com>
2015-10-27 15:22:08 -0700

    Update continuous_time_coalescent.py

--- continuous_time_coalescent.py
+++ continuous_time_coalescent.py
@@ -13,17 +13,17 @@ k = len(N)
 
 while k > 1:
 
-	T = exp(k*(k-1)/2.)
+	T = exp(2./(k*(k-1)))
 
-	ix = sample(range(k),2,replace=False)
+	ix = sample(range(k), 2, replace=False)
 
 	g0 = [item for i,item in enumerate(N) if i not in ix]
 	g1 = [N[i] for i in ix]
 
 	g0.append(g1)
 	N = g0
+	
+	k = k-1
 
 	print T, N
-
-	k = k-1
 
